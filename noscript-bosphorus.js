c0322cf44ecb48ea04c847eb6b829263c3a26a3f
Aleksei Androsov <aandrosov@yandex-team.ru>
2015-04-21 20:17:20 +0300

    Микрооптимизации
    Убрал apply, try-catch вынес в функцию, унифицировал код

--- noscript-bosphorus.js
+++ noscript-bosphorus.js
@@ -8,25 +8,16 @@
 
     /**
      * Хелпер для фактического вызова метода модели и обработки результата
-     * @param {ns.View} view вид, инициирующий вызов
+     * @param {ns.View~UpdateTree} updateTree вид, инициирующий вызов
      * @param {String} methodName имя метода модели
      * @param {...*} args
      * @returns {*}
      */
-    function nsViewCallHelper(view, methodName, p1, p2, p3, p4, p5) {
-        var tree = yr.nodeset2data(view);
-        view = tree.bosphorus;
+    function nsViewCallHelper(updateTree, methodName, p1, p2, p3, p4, p5) {
+        var tree = yr.nodeset2data(updateTree);
+        var view = tree.bosphorus;
 
-        try {
-            var result = view[methodName](p1, p2, p3, p4, p5);
-        } catch (e) {
-            ns.log.exception('ns-view-call', e, {
-                id: view.id,
-                key: view.key,
-                method: methodName
-            });
-            return yr.object2nodeset({});
-        }
+        var result = safeViewCall(view, methodName, p1, p2, p3, p4, p5);
 
         if (Array.isArray(result)) {
             return yr.array2nodeset(result);
@@ -39,69 +30,82 @@
      * Мост в JS для вызова метода вида
      * @private
      */
-    yr.externals['_ns-view-call'] = function () {
-        return nsViewCallHelper.apply(null, arguments);
-    };
+    yr.externals['_ns-view-call'] = nsViewCallHelper;
 
     /**
      * Мост в JS для вызова метода вида
      * @private
      */
-    yr.externals['_ns-view-call-scalar'] = function () {
-        return nsViewCallHelper.apply(null, arguments);
-    };
+    yr.externals['_ns-view-call-scalar'] = nsViewCallHelper;
 
     /**
      * Хелпер для фактического вызова метода модели и обработки результата
-     * @param {ns.View~UpdateTree} view вид, инициирующий вызов
+     * @param {ns.View~UpdateTree} updateTree вид, инициирующий вызов
      * @param {String} modelName имя класса модели
      * @param {String} methodName имя метода модели
      * @param {...*} args
      * @returns {*}
      */
-    function nsModelCallHelper(tree, modelName, methodName, p1, p2, p3, p4, p5) {
-        tree = yr.nodeset2data(tree);
+    function nsModelCallHelper(updateTree, modelName, methodName, p1, p2, p3, p4, p5) {
+        var tree = yr.nodeset2data(updateTree);
         /** @type ns.View */
         var view = tree.bosphorus;
 
         var model = view.getModel(modelName);
-        if (model) {
+        ns.assert(model, 'noscript-bosphorus', 'Model is undefined');
 
-            try {
-                var result = model[methodName](p1, p2, p3, p4, p5);
-            } catch (e) {
-                ns.log.exception('ns-model-call', e, {
-                    id: view.id,
-                    key: view.key,
-                    method: methodName,
-                    model: modelName
-                });
-                return yr.object2nodeset({});
-            }
+        var result = safeModelCall(view, model, methodName, p1, p2, p3, p4, p5);
 
-            if (Array.isArray(result)) {
-                return yr.array2nodeset(result);
-            } else {
-                return yr.object2nodeset(result);
-            }
+        if (Array.isArray(result)) {
+            return yr.array2nodeset(result);
+        } else {
+            return yr.object2nodeset(result);
         }
-
-        return yr.object2nodeset({});
     }
 
     /**
      * Мост в JS для вызова метода модели вида
      * @private
      */
-    yr.externals['_ns-model-call'] = function(tree, modelName, modelMethod, p1, p2, p3, p4, p5) {
-        return nsModelCallHelper(tree, modelName, modelMethod, p1, p2, p3, p4, p5);
-    };
+    yr.externals['_ns-model-call'] = nsModelCallHelper;
 
     /**
      * Мост в JS для вызова метода модели вида
      * @private
      */
-    yr.externals['_ns-model-call-scalar'] = function() {
-        return nsModelCallHelper.apply(null, arguments);
-    };
+    yr.externals['_ns-model-call-scalar'] = nsModelCallHelper;
+
+    /**
+     * Безопасно вызываем метод вида
+     */
+    function safeViewCall(view, methodName, p1, p2, p3, p4, p5) {
+        try {
+            return view[methodName](p1, p2, p3, p4, p5);
+        } catch (e) {
+            ns.log.exception('ns-view-call', e, {
+                id: view.id,
+                key: view.key,
+                method: methodName
+            });
+            return [];
+        }
+    }
+
+    /**
+     * Безопасно вызываем метод модели
+     */
+    function safeModelCall(view, model, methodName, p1, p2, p3, p4, p5) {
+        try {
+            return model[methodName](p1, p2, p3, p4, p5);
+        } catch (e) {
+            ns.log.exception('ns-model-call', e, {
+                id: view.id,
+                key: view.key,
+                method: methodName,
+                model: model.id
+            });
+            return [];
+        }
+    }
+
 })();
